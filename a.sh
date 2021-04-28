697a2e6372a44dabdb4d64874feb975b1a54f6e2
VectorTensor <comppro54@gmail.com>
2021-04-28 08:44:59 +0545

    Fixed a bug

--- a.sh
+++ a.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DATA1=('noodles' 'sugar')
+DATA1=('noodles good one' 'sugar')
 # Here you have to enter the data you want to post to the api.DATA1 =Things DATA2 = Money. Don't enter comma between
 #two items they are separated by spaces
 
@@ -8,7 +8,7 @@ a=${#DATA1[@]}
 var1=0
 while [ $var1 -lt $a ]
 do
-	./example.sh ${DATA1[$var1]} ${DATA2[$var1]}
+	./example.sh "${DATA1[$var1]}" ${DATA2[$var1]}
 	var1=$[ $var1 + 1 ] 
 
 
