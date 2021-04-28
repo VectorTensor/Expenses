a147144ddd7e7ce16dafe6a719e7ae74c857cf07
VectorTensor <68182225+VectorTensor@users.noreply.github.com>
2021-04-28 08:40:45 +0545

    Update example.sh

--- example.sh
+++ example.sh
@@ -5,4 +5,4 @@ url='http://127.0.0.1:8000/api/'
 data="{\"Things\":\"$a\",\"Money\":$b}"
 echo "$data"
 echo
-curl $url -X POST -d $data -H  'content-type:application/json'
+curl $url -X POST -d "$data" -H  'content-type:application/json'
